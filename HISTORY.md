9f72ac726a175f9e7105f729e76ae3969cf4380a
Bart Eenkhoorn <47192048+bjeenkhoorn@users.noreply.github.com>
2020-06-04 15:49:39 +0200

    Add files via upload

---
+++ HISTORY.md
@@ -0,0 +1,29 @@
+Clavier duun du Mali (Qwerty) Keyboard Change History
+=======================
+
+1.0 4-6-2020 Duungooma qwerty keyboard 
+
+-----------------
+
+4.0 (2020-02-26)
+----------------
+* Updated to support new characters
+
+3.3 (12-Sep-2018)
+------------------
+* Moved to open github system
+
+3.2 (19-Oct-2017)
+-----------------
+* Updates to keyboard by Dan Brubaker
+
+2.2 (2008)
+* Updated keyboard by Dan Brubaker
+
+(Nov-2007)
+------
+* Composed sequences were added by Dan Brubaker
+
+(2004)
+------
+*  Based on keyboard for Burkina Faso by Doug Higby
---
+++ source/help/duun_qwerty.php
@@ -0,0 +1,25 @@
+<?php 
+  $pagename = 'Clavier duun du Mali (qwerty) aide';
+  $pagetitle = $pagename;
+  // Header we will tidy up later  
+  require_once('header.php');
+?>
+
+<p>
+    Le clavier duun vous permet de saisir du texte dans votre langue maternelle !
+</p>
+
+<p>Voir <a href="file:DuunTableauTouchesQWertyCouleur.pdf">cette document.</a>  Cette page explique comment saisir du texte en langue duun du Mali.</p>
+
+<h1>Clavier duun qwerty de base</h1>
+
+<p><b>Clavier de base</b></p>
+<img src='duun_all.png' alt='Keyboard Layout'>
+<p><b>Clavier à l’écran</b></p>
+<img src='duun_shift.png' alt='Keyboard Layout'>
+
+<h2>Disposition du clavier du téléphone / Phone keyboard layout</h2>
+
+<div id='osk-phone' data-states='default shift numeric symbol'>
+</div>
+
